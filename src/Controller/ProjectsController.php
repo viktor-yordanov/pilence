5f65cd3dde0ef5789597ab00242fc6d46e95af8d
Viktor Yordanov <127318378+viktor-yordanov@users.noreply.github.com>
2025-07-03 23:22:17 +0300

    Restrict project actions to admin

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -2,12 +2,10 @@
 
 namespace App\Controller;
 
-use App\Form\ProjectForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProjectRepository;
-use Symfony\Component\HttpFoundation\Request;
 
 class ProjectsController extends AbstractController
 {
@@ -22,29 +20,6 @@ class ProjectsController extends AbstractController
         ]);
     }
 
-    #[Route('/projects/new', name: 'project_new')]
-    public function new(Request $request, ProjectRepository $projectRepository): Response
-    {
-        $form = $this->createForm(ProjectForm::class);
-
-        // Handle the form submission
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            // Process the form data, e.g., save the project to the database
-            $project = $form->getData();
-            // Assuming you have a service or repository to handle saving
-            $projectRepository->add($project, true);
-
-            // Redirect to the project list or detail page after saving
-            return $this->redirectToRoute('projects');
-        }
-
-        // Render a form to create a new project
-        return $this->render('projects/new.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
-
     #[Route('/projects/{id}', name: 'project_details')]
     public function detail(int $id, ProjectRepository $projectRepository): Response
     {
@@ -56,25 +31,6 @@ class ProjectsController extends AbstractController
         }
 
         return $this->render('projects/details.html.twig', [
-            'project' => $project,
-        ]);
-    }
-
-    #[Route('/projects/{id}/edit', name: 'project_edit')]
-    public function edit(int $id, ProjectRepository $projectRepository): Response
-    {
-        // Fetch the project to edit
-        $project = $projectRepository->find($id);
-
-        if (!$project) {
-            throw $this->createNotFoundException('Project not found');
-        }
-
-        $form = $this->createForm(ProjectForm::class, $project);
-
-        // Render a form to edit the project
-        return $this->render('projects/edit.html.twig', [
-            'form' => $form->createView(),
             'project' => $project,
         ]);
     }
